2ad8ecbc883a91c6a678e13f5d42df845eb89992
Mathew Thomas <mattazoid@gmail.com>
2018-10-16 20:25:57 -0400

    2nd test

---
+++ test/test2.js
@@ -0,0 +1,14 @@
+var expect = require('chai').expect;
+var addIt = require('../addIt');
+
+describe('addIt()', function () {
+  it('should add two numbers', function () {
+    var num1 = 1;
+    var num2 = 1;
+    var expected = num1 + num2;
+    console.log("expected sum = " + expected);
+
+    var result = addIt(num1, num2);
+    expect(result).to.be.equal(expected);
+  });
+});
