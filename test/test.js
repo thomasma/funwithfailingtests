a6a972b4f1b628436d7fb4fa0dde3866ce390788
Mathew Thomas <mattazoid@gmail.com>
2018-10-16 20:18:11 -0400

    update

--- test/test.js
+++ test/test.js
@@ -1,10 +1,15 @@
-require('volkswagen')
+//require('volkswagen')
 var expect = require('chai').expect;
 var addIt = require('../addIt');
 
 describe('addIt()', function () {
   it('should add two numbers', function () {
-    var sum = addIt(5, 1);
-    expect(sum).to.be.equal(6);
+    var num1 = 5;
+    var num2 = 1;
+    var expected = num1 + num2;
+    console.log("expected sum = " + expected);
+
+    var result = addIt(num1, num2);
+    expect(result).to.be.equal(expected);
   });
 });
